06f428f00a2c30bb731fbbeb8b0ee08bf0022ee5
JJKeiser <jjluengo.sp@gmail.com>
2023-10-20 18:21:45 +0200

    dsds

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+    private float inputH;
+    private float inputV;
+    [SerializeField] private float strengh;
+
+    private Vector3 spawnPoint;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb= GetComponent<Rigidbody>();
+
+        spawnPoint = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        inputV = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //ForceMode.Force: fuerza continua (FIXED UPDATE)
+        //ForceMode.Impulse: fuerza INSTANTÁNEA.
+        rb.AddForce(new Vector3(inputH, 0, inputV) * strengh ,ForceMode.Impulse);
+    }
+}
