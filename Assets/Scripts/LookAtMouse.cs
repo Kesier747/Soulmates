e3bac871f6d5406e2ab5b62978ead41487a90ce1
JJKeiser <jjluengo.sp@gmail.com>
2023-10-20 18:43:14 +0200

    l

--- Assets/Scripts/LookAtMouse.cs
+++ Assets/Scripts/LookAtMouse.cs
@@ -1,9 +1,14 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour
 {
+    public GameObject crosshair;
+    public GameObject player;
+    private Vector3 target;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,10 @@ public class LookAtMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        target = transform.GetComponent<Camera>().ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, transform.position.z));
+
+        Vector3 diff = target - player.transform.position;
+        float rotationY = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        player.transform.rotation = Quaternion.Euler(0.0f, rotationY, 0.0f);
     }
 }
