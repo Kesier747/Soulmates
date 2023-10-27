d853d981b10c554933863d3e7eaff7c570ae80b3
Kesier747 <jjluengo.sp@gmail.com>
2023-10-27 19:22:29 +0200

    fsdfsd

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -10,9 +10,16 @@ public class PlayerInput : MonoBehaviour
     private float inputH;
     private float inputV;
     [SerializeField] private float speed;
+    [SerializeField] private float sprintSpeed;
+    [SerializeField] private float normalSpeed;
+    [SerializeField] private float stamina;
 
     private Vector3 spawnPoint;
-    
+
+    [SerializeField] private Camera camera;
+    [SerializeField] private LayerMask whatIsGround;
+    private float RayLength;
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,20 +27,32 @@ public class PlayerInput : MonoBehaviour
         rb= GetComponent<Rigidbody>();
 
         spawnPoint = transform.position;
+
+        camera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            speed = sprintSpeed;
+        }
+        else
+        {
+            speed = normalSpeed;
+        }
+
         inputH = Input.GetAxisRaw("Horizontal");
         inputV = Input.GetAxisRaw("Vertical");
 
         transform.Translate(new Vector3(inputH, 0, inputV).normalized * speed * Time.deltaTime);
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+        Ray cameraRay = camera.ScreenPointToRay(Input.mousePosition);
+        Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
 
-        }
+        if (groundPlane.Raycast(cameraRay, out )) ;
+        
     }
 
     //private void FixedUpdate()
