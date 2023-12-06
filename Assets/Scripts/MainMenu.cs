28d5953c1cb4e591bb721301cb5ba81b38311b27
JJKeiser <jjluengo.sp@gmail.com>
2023-12-06 18:02:29 +0100

    si

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2);
         Time.timeScale = 1.0f;
     }
 
