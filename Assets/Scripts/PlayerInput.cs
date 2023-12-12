2966ab1dabc3974753186f0b9154179a31c9fc55
JJKeiser <jjluengo.sp@gmail.com>
2023-12-12 13:01:34 +0100

    petar el suelo de cosas

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerInput : MonoBehaviour
 {
@@ -12,7 +13,8 @@ public class PlayerInput : MonoBehaviour
     private float inputV;
 
     private float gravity = -9.81f;
-    [SerializeField] private float gravityMultiplier;
+    [SerializeField] private float gravityMultiplier = 3.0f;
+    private float velocity;
     private Vector3 verticalMovement;
  
     [SerializeField] private float speed;
@@ -48,12 +50,10 @@ public class PlayerInput : MonoBehaviour
         {
             Shoot();
         }
-        inputH = Input.GetAxisRaw("Horizontal");
-        inputV = Input.GetAxisRaw("Vertical");
-       
-        controller.Move(new Vector3(inputH, 0, inputV).normalized * speed * Time.deltaTime);
 
-        Gravity();
+        ApplyMovement();
+        ApplyRotation();
+        ApplyGravity();
 
         //Para el dash y la corrutina del dash
         if (Input.GetKeyDown(KeyCode.LeftShift) && !dashing)
@@ -61,16 +61,34 @@ public class PlayerInput : MonoBehaviour
             StartCoroutine(Dash());
         }
 
+    }
+
+    private void ApplyMovement()
+    {
+        inputH = Input.GetAxisRaw("Horizontal");
+        inputV = Input.GetAxisRaw("Vertical");
+
+        controller.Move(new Vector3(inputH, 0, inputV).normalized * speed * Time.deltaTime);
+    }
+
+    private void ApplyRotation()
+    {
         //Todo esto para que el Player mire en dirección de donde la cámara este apuntando
         Ray cameraRay = mainCamera.ScreenPointToRay(Input.mousePosition);
-        if(Physics.Raycast(cameraRay, out RaycastHit hit, 1000, whatIsGround) && Time.timeScale!= 0)
+        if (Physics.Raycast(cameraRay, out RaycastHit hit, 1000, whatIsGround) && Time.timeScale != 0)
         {
             Vector3 directionToLook = (hit.point - transform.position).normalized;
             directionToLook.y = 0; //No cambio la altura.
             Quaternion rotationToLook = Quaternion.LookRotation(directionToLook);
             transform.rotation = rotationToLook; //La rotación del Player
-                //Debug.DrawRay(cameraRay.origin, cameraRay.direction * 500, Color.red, 10);
+                                                 //Debug.DrawRay(cameraRay.origin, cameraRay.direction * 500, Color.red, 10);
         }
+    }
+
+    private void ApplyGravity()
+    {
+        velocity = gravity * gravityMultiplier;
+        controller.Move (new Vector3(0,velocity,0) * Time.deltaTime);
     }
 
     private void Shoot() //Pues pa disparar
@@ -89,12 +107,6 @@ public class PlayerInput : MonoBehaviour
         controller.Move(new Vector3(inputH, 0, inputV).normalized * dashPower * Time.deltaTime);
         yield return dashing = false;
 
-    }
-
-    private void Gravity()
-    {
-        //verticalMovement.y += Physics.gravity.y * gravityFactor * Time.deltaTime;
-        //controller.Move(verticalMovement * Time.deltaTime);
     }
 
 }
