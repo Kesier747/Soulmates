982b559c44632c977fb837175c4243c054bd9ea1
JJKeiser <jjluengo.sp@gmail.com>
2024-06-23 21:00:05 +0200

    mas tests

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
         Time.timeScale = 1.0f;
     }
 
